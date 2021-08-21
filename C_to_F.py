9153b551795d9efe593ffe012d125682b357cf56
Hsin Yu <winter0462@gmail.com>
2021-08-21 16:47:47 +0800

    Add new file

---
+++ C_to_F.py
@@ -0,0 +1,7 @@
+# 攝氏轉換華氏公式: F = C * 9 / 5 + 32
+
+Celsius = input('請輸入攝氏溫度(數字): ')
+Celsius = float(Celsius)
+Fahrenheit = Celsius * 9 / 5 + 32
+
+print('轉換結果: ', Celsius , '°C', ';', Fahrenheit, '℉')
